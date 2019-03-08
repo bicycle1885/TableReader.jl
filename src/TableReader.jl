7a2ccf0ed41e2c8de829c05153d0f373b8fa4b4b
Kenta Sato <bicycle1885@gmail.com>
2019-03-08 09:35:25 +0900

    add simple readtsv

--- src/TableReader.jl
+++ src/TableReader.jl
@@ -1,5 +1,126 @@
 module TableReader
 
-greet() = print("Hello World!")
+export readtsv
+
+using DataFrames:
+    DataFrame
+using TranscodingStreams:
+    TranscodingStream,
+    NoopStream,
+    Memory,
+    buffermem
+
+function readtsv(filename::AbstractString)
+    return open(readtsv, filename)
+end
+
+function readtsv(file::IO)
+    return readtsv(NoopStream(file, bufsize = 128 * 2^20))
+end
+
+function readtsv(stream::TranscodingStream)
+    header = readline(stream)
+    colnames = Symbol.(split(header, '\t'))
+    ncols = length(colnames)
+    columns = [sizehint!(Any[], 1000) for _ in 1:ncols]
+    tokens = Vector{UnitRange{Int}}(undef, ncols)
+    state = ParserState(stream, 1, columns, tokens)
+    delim = UInt8('\t')
+    while !eof(stream)
+        mem = buffermem(stream.state.buffer1)
+        Δ = scanline!(state, mem, delim)
+        if Δ == 0
+            # TODO
+            @assert false
+        end
+        for i in 1:length(tokens)
+            val = parse_integer(mem, tokens[i])
+            push!(columns[i], val)
+        end
+        skip(stream, Δ)
+    end
+    return DataFrame(columns, colnames)
+end
+
+@inline function parse_integer(mem, range)
+    n::Int = 0
+    for i in range
+        @inbounds b = mem[i]
+        n = 10n + (b - UInt8('0'))
+    end
+    return n
+end
+
+mutable struct ParserState
+    stream::TranscodingStream
+    line::Int
+    columns::Vector{Vector}
+    tokens::Vector{UnitRange{Int}}
+end
+
+struct ReadError <: Exception
+    msg::String
+    line::Int
+end
+
+macro state(name)
+    esc(quote
+        @label $(name)
+        pos += 1
+        if pos > pos_end
+            @goto ENDFILE
+        end
+        @inbounds c = mem[pos]
+        #@show Char(c)
+    end)
+end
+
+# Scan a line in mem; mem must include one or more lines.
+function scanline!(state::ParserState, mem::Memory, delim::UInt8)
+    pos = 0
+    pos_end = lastindex(mem)
+    pos_token = 0
+    i = 1
+
+    @state STATE_BEGIN
+    if UInt8('0') ≤ c ≤ UInt8('9')
+        pos_token = pos
+        @goto STATE_INTEGER
+    elseif c == UInt8('\n')
+        @goto ENDLINE
+    end
+    @goto ERROR
+
+    @state STATE_INTEGER
+    if UInt8('0') ≤ c ≤ UInt8('9')
+        @goto STATE_INTEGER
+    elseif c == delim
+        state.tokens[i] = pos_token:pos-1
+        i += 1
+        @goto STATE_DELIM
+    elseif c == UInt8('\n')
+        state.tokens[i] = pos_token:pos-1
+        i += 1
+        @goto ENDLINE
+    end
+    @goto ERROR
+
+    @state STATE_DELIM
+    if UInt8('0') ≤ c ≤ UInt8('9')
+        pos_token = pos
+        @goto STATE_INTEGER
+    end
+    @goto ERROR
+
+    @label ERROR
+    throw(ReadError("invalid file format at line $(state.line)", state.line))
+
+    @label ENDFILE
+    return 0
+
+    @label ENDLINE
+    state.line += 1
+    return pos
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using TableReader
 using Test
 
-@testset "TableReader.jl" begin
-    # Write your own tests here.
+@testset "readtsv" begin
+    buffer = IOBuffer("""
+    col1\tcol2\tcol3
+    1\t2\t3
+    4\t5\t6
+    """)
+    df = readtsv(buffer)
+    @test eof(buffer)
+    @test names(df) == [:col1, :col2, :col3]
+    @test df[:col1] == [1, 4]
+    @test df[:col2] == [2, 5]
+    @test df[:col3] == [3, 6]
 end
