91da5bb432bdf8469acccd634b5d4ada6057f119
Kenta Sato <bicycle1885@gmail.com>
2019-03-22 13:54:21 +0900

    add tools/profile.jl

---
+++ tools/profile.jl
@@ -0,0 +1,18 @@
+using TableReader
+
+# Measure elapsed time.
+filename = ARGS[1]
+reader = endswith(filename, ".csv") ? readcsv :
+         endswith(filename, ".tsv") ? readtsv :
+         error("the extension of the filename must .csv or .tsv")
+if length(ARGS) ≥ 2
+    kwargs = eval(Meta.parse(ARGS[2]))
+end
+for _ in 1:3
+    @time reader(filename; kwargs...)
+end
+
+# Profile.
+using Profile
+@profile reader(filename; kwargs...)
+Profile.print(IOContext(stdout, :displaysize => (10000, 10000)))
