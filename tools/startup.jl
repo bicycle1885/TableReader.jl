d2d50c4850756832a8060befc0eaaa17e8a57a7e
Kenta Sato <bicycle1885@gmail.com>
2019-04-03 01:25:21 +0900

    add package load time to tools/startup.jl

--- tools/startup.jl
+++ tools/startup.jl
@@ -1,10 +1,12 @@
-using TableReader
+print("Package load time:")
+@time using TableReader
 
 if length(ARGS) != 1
     error("a function name is expected")
 end
 
 func = ARGS[1]
+print("Calling $(func) time:")
 if func === "readcsv"
     @time readcsv("test/test.csv")
 elseif func === "readtsv"
